40b58c899323107013b5036e60ce2dc533d8388d
Nguyễn Quốc Cường <cgvnd123@gmail.com>
2018-08-24 11:35:36 +0700

    db seed

---
+++ manage_user/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=1 ; $i<=10 ; $i++){
+            DB::table('users')->insert([
+                'id' => str_random(10),
+                'first_name' => str_random(10),
+                'last_name' => str_random(10),
+                'role' => rand(0,1),
+                'password' => bcrypt('secret'),
+            ]);
+        }
+    }
+}
