cad3e2c8fa079b3384a38617f9c763dc3f072093
Nguyễn Quốc Cường <cgvnd123@gmail.com>
2018-08-27 15:09:38 +0700

    index page view

--- manage_user/resources/views/index.blade.php
+++ manage_user/resources/views/index.blade.php
@@ -1,21 +1,31 @@
 @include ('header')
 <body>
     <div class="container">
-        <h1 class="text-center">User Management</h1>
+        <h1 class="text-left">User Management</h1>
+        <div class="profile">
+            <i class="far fa-user"> {{\Cookie::get('email')}}</i>
+            <a href="{{url('logout')}}"><i class="fas fa-sign-out-alt "></i></a>
+        </div>
         <button class="btn-primary add-btn" data-toggle="modal" data-target="#add-user-modal"> Add User</button>
         <table class="row table">
-            <th class="col-md-3 text-center">ID</th>
-            <th class="col-md-4 text-center">Name</th>
+            <th class="col-md-1 text-center">STT</th>
+            <th class="col-md-2 text-center">ID</th>
+            <th class="col-md-3 text-center">Email</th>
+            <th class="col-md-2 text-center">Name</th>
             <th class="col-md-2 text-center">Role</th>
+            @php($i=1)
             @foreach ($users as $user)
             <tr>
+                <td class="text-center">{{$i}}</td>
                 <td class="text-center">{{$user->user_id}}</td>
-                <td class="text-center">{{$user->first_name}} {{$user->last_name}}</td>
+                <td class="text-center">{{$user->email}}</td>
+                <td class="text-center">{{$user->user_name}}</td>
                 <td class="text-center">{{$user->role}}</td>
                 <td> <button class="btn-primary edit-btn">Edit</button></td>
                 <td> <button class="btn-danger delete-btn">Delete</button></td>
                 <td></td>
             </tr>
+            @php($i++)
             @endforeach
         </table>
     </div>
