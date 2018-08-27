2cb4174443d292052dca44f1344b0f4e85abd40a
Nguyễn Quốc Cường <cgvnd123@gmail.com>
2018-08-27 15:04:13 +0700

    Add Enums

---
+++ manage_user/app/Enums/Message.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cuongnq
+ * Date: 27/08/2018
+ * Time: 14:42
+ */
+
+namespace App\Enums;
+
+
+use MyCLabs\Enum\Enum;
+
+class Message extends Enum
+{
+    const IN_VALID_LOGIN = 'Invalid Email or Password';
+}
---
+++ manage_user/app/Enums/Role.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cuongnq
+ * Date: 27/08/2018
+ * Time: 14:45
+ */
+
+namespace App\Enums;
+
+
+use MyCLabs\Enum\Enum;
+
+class Role extends Enum
+{
+    const ADMIN = 1;
+    const USER = 0;
+}
