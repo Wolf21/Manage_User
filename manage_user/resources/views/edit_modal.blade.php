1914fa402d87ee6afce2622faa0c67732b9b8aa3
Nguyễn Quốc Cường <cgvnd123@gmail.com>
2018-08-27 15:05:20 +0700

    css for Edit modal

--- manage_user/resources/views/edit_modal.blade.php
+++ manage_user/resources/views/edit_modal.blade.php
@@ -3,15 +3,31 @@
              aria-labelledby="myModalLabel">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <div class="modal-body">
-                <label class="label-input">User ID</label>
-                <input type="text" name="user_id" class="input-modal">
-                <label class="label-input">First Name</label>
-                <input type="text" name="first_name" class="input-modal">
-                <label class="label-input" >Last Name</label>
-                <input type="text" name="last_name" class="input-modal">
-                <label class="label-input">Role</label>
-                <input type="text" name="role" class="input-modal">
+            <div class="modal-body row">
+                <div class="col-md-5 row">
+                    <label class="label-input col-md-4">User ID</label>
+                    <input type="text" name="user_id" class="input-modal col-md-6">
+                </div>
+                <div class="col-md-5 row">
+                    <label class="label-input col-md-4">Role</label>
+                    <input type="text" name="role" class="input-modal col-md-6">
+                </div>
+                <div class="col-md-5 row">
+                    <label class="label-input col-md-4">Email</label>
+                    <input type="text" name="first_name" class="input-modal col-md-6">
+                </div>
+                <div class="col-md-5 row">
+                    <label class="label-input col-md-4" >Name</label>
+                    <input type="text" name="last_name" class="input-modal col-md-6">
+                </div>
+                <div class="col-md-5 row">
+                    <label class="label-input col-md-4" >Password</label>
+                    <input type="password" name="last_name" class="input-modal col-md-6">
+                </div>
+                <div class="col-md-5 row">
+                    <label class="label-input col-md-4" >Retype Pass</label>
+                    <input type="password" name="last_name" class="input-modal col-md-6">
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
