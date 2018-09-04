3c782b4704173bd833154c6fdacd2d1ef5707e7e
Nguyễn Quốc Cường <cgvnd123@gmail.com>
2018-09-04 17:06:49 +0700

    make factory and setup to test

--- manage_user/tests/TestCase.php
+++ manage_user/tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Default preparation for each test
+     *
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
 }
